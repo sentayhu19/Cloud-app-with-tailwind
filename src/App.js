bcdd3aec0a784a5776cbd11ba38363265f52712f
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-20 23:37:07 +0300

    Nav bar created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1 class="text-4xl">Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
