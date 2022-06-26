e9828ec1bddb2cdb036cb1015e98f5c7d5a0ded2
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-26 19:03:44 +0300

    Support section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Support from './components/Support';
 
 const App = () => (
   <div className="App">
     <Navbar />
     <Hero />
     <About />
+    <Support/>
   </div>
 );
 
