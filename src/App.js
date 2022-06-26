ff17b250375ca15549512e5eb18296692b2cab9e
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-26 23:23:18 +0300

    Linters error fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -6,23 +8,22 @@ import Support from './components/Support';
 import Allinone from './components/Allinone';
 import Pricing from './components/Pricing';
 import Footer from './components/Footer';
-import Aos from 'aos';
-import "aos/dist/aos.css";
+
 const App = () => {
-useEffect(()=>{
-  Aos.init({duration: 1000, delay:150})
-},[]);
-return (
-  <div className="App">
-    <Navbar />
-    <Hero />
-    <About />
-    <Support />
-    <Allinone />
-    <Pricing />
-    <Footer />
-  </div>
-);
-}
+  useEffect(() => {
+    Aos.init({ duration: 1000, delay: 150 });
+  }, []);
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <Allinone />
+      <Pricing />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
